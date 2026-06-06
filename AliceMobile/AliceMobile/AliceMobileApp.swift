879fa78649be6b4c61cd34db1a2fcb1e3fe59a99
fangwendongcs <89453496+fangwendongcs@users.noreply.github.com>
2026-06-06 19:02:41 +0800

    Initial iOS project

---
+++ AliceMobile/AliceMobile/AliceMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliceMobileApp.swift
+//  AliceMobile
+//
+//  Created by 方文栋 on 2026/6/6.
+//
+
+import SwiftUI
+
+@main
+struct AliceMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AliceMobile/AliceMobileTests/AliceMobileTests.swift
@@ -0,0 +1,19 @@
+//
+//  AliceMobileTests.swift
+//  AliceMobileTests
+//
+//  Created by 方文栋 on 2026/6/6.
+//
+
+import Testing
+@testable import AliceMobile
+
+struct AliceMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
